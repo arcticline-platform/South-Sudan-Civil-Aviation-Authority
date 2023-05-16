8b15248502d664e6833cdb1f65194a8417a3cb24
okumu daniel comboni <okumucomboni@gmail.com>
2023-05-16 17:48:53 +0300

    project in progress

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import {Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './layout/Layout'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+            <Route path='/' element={<Layout />}>
+              <Route path="/" element={<Home />} />
+            </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
