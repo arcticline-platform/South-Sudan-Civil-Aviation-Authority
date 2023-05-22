ff72790129fb91eb0dd45b5621c422518708f493
okumu daniel comboni <okumucomboni@gmail.com>
2023-05-22 13:05:05 +0300

    error problem

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-module.exports = {
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
       external: ['react-dom/client']
     }
   }
-}
+})
